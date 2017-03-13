4fe46b09547698f094c87b60f3fd6ab23a7679f1
William Bernting <william.bernting@kits.se>
2017-03-13 09:35:16 +0100

    change wildcard in pkill to use regex wildcard

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function moveOldDeployFile() {
     execSyncEx(`mv -vf ${argv.filename} ${argv.productionfilename}`);
 
     // Kill running server & start a new one
-    execSyncEx('pkill -9 -f gakusei*.jar');
+    execSyncEx('pkill -9 -f gakusei.*.jar');
 
     // DB backup
     execSyncEx(`pg_dump -U gakusei -h 127.0.0.1 -o gakusei > ${retireDirname}/db_backup_${currentDate}.sql`);
